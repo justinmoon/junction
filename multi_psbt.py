32128f62f8c5bb3739a816344ca6cdc3f0d83819
Justin Moon <mail@justinmoon.com>
2019-07-15 17:18:40 -0500

    Multi20f2 class

--- multi_psbt.py
+++ multi_psbt.py
@@ -1,3 +1,6 @@
+import json
+import os.path
+
 from bitcoinlib.keys import HDKey
 
 from rpc import RPC
@@ -5,60 +8,86 @@ from rpc import RPC
 
 rpc = RPC()
 
-def generate_keys():
-    with open("secret1.txt", "w") as f:
-        hd1 = HDKey(network="testnet")
-        f.write(hd1.wif_private())
-    with open("secret2.txt", "w") as f:
-        hd2 = HDKey(network="testnet")
-        f.write(hd2.wif_private())
 
+class Multi2of2:
 
-def load_keys():
-    # get private keys
-    with open("secret1.txt") as f:
-        hd1 = HDKey(f.read(), network="testnet")
-    with open("secret2.txt") as f:
-        hd2 = HDKey(f.read(), network="testnet")
-    return hd1, hd2
+    filename = "state.json"
 
+    def __init__(self, key1, key2, next_index):
+        self.key1 = key1
+        self.key2 = key2
+        self.next_index = next_index
+        self.rpc1 = RPC("multi-1")
+        self.rpc2 = RPC("multi-2")
+        self.chunk = 10
 
-def derive_addresses(key1, key2):
-    descriptor = descriptor_2_of_2(key1, key2)
-    return rpc.deriveaddresses(descriptor, [0, 5])
+    @classmethod
+    def init(cls):
+        if os.path.exists(cls.filename):  # FIXME
+            raise RuntimeError("state.json already exists")
+        key1 = HDKey(network="testnet")
+        key2 = HDKey(network="testnet")
+        next_index = 0
+        multi = cls(key1, key2, next_index)
+        multi.commit()
+        multi.export_bitcoind()
+        return multi
 
+    @classmethod
+    def load(cls):
+        with open(cls.filename) as f:
+            state = json.load(f)
+            return cls(
+                HDKey(state["xprv1"]),
+                HDKey(state["xprv2"]),
+                state["next_index"],
+            )
 
-def descriptor_2_of_2(key1, key2):
-    xpub1 = key1.wif_public()
-    xpub2 = key2.wif_public()
-    raw_descriptor = f"wsh(multi(2,{xpub1}/*,{xpub2}/*))"
-    descriptor = rpc.getdescriptorinfo(raw_descriptor)['descriptor']
-    return descriptor
+    def commit(self):
+        state = {
+            "xprv1": self.key1.wif_private(),
+            "xprv2": self.key2.wif_private(),
+            "next_index": self.next_index,
+        }
+        with open(self.filename, "w") as f:
+            json.dump(state, f)
 
+    def address(self):
+        return rpc.deriveaddresses(
+            self.descriptor(), 
+            [self.next_index, self.next_index + 1])[0]
 
-def import_addresses(key1, key2):
-    rpc1 = RPC("multi-1")
-    rpc2 = RPC("multi-2")
-    descriptor = descriptor_2_of_2(key1, key2)
-    import1 = rpc1.importmulti([{
-        "desc" : descriptor, 
-        "range" : [0, 1000], 
-        "watchonly" : True, 
-        "timestamp" : "now",
-    }])
-    print("import1 result:", import1)
-    import2 = rpc2.importmulti([{
-        "desc" : descriptor, 
-        "range" : [0, 1000], 
-        "watchonly" : True, 
-        "timestamp" : "now",
-    }])
-    print("import2 result:", import2)
+    def descriptor(self):
+        xpub1 = self.key1.wif_public()
+        xpub2 = self.key2.wif_public()
+        raw_descriptor = f"wsh(multi(2,{xpub1}/*,{xpub2}/*))"
+        # appends checksum to descriptor
+        return rpc.getdescriptorinfo(raw_descriptor)['descriptor']
+
+    def export_bitcoind(self):
+        descriptor = self.descriptor()
+        for rpc in (self.rpc1, self.rpc2):
+            address_range = [self.next_index, self.next_index + self.chunk]
+            result = rpc.importmulti([{
+                "desc" : descriptor, 
+                "range" : address_range, 
+                "watchonly" : True, 
+                "timestamp" : "now",
+            }])
+            if not result[0]["success"]:
+                raise RuntimeError(f"Bitcoind export failed: {result}")
+            print("Bitcoind export successful")
 
 
 def main():
-    key1, key2 = load_keys()
-    import_addresses(key1, key2)
+    if not os.path.exists(Multi2of2.filename):
+        print("Initializing 2-of-2 multisig. This might take a while")
+        multi = Multi2of2.init()
+    else:
+        print("Loading 2-of-2 multisig from disk")
+        multi = Multi2of2.load()
+
+    print(multi.address())
 
 if __name__ == '__main__':
     main()
