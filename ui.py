91d861e3588803ae29cc3f7286e02832bd50ee82
Justin Moon <mail@justinmoon.com>
2019-08-10 23:33:45 -0500

    create wallet ui

--- ui.py
+++ ui.py
@@ -8,9 +8,11 @@ def index():
     devices = commands.enumerate()
     return render_template('index.html', devices=str(devices))
 
-@app.route('/devices')
+@app.route('/create-wallet')
+def create_wallet():
+    return render_template('create-wallet.html')
+
+@app.route('/api/devices')
 def devices():
     devices = commands.enumerate()
     return jsonify(devices)
-
-
