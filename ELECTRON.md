9b190512b9a225bdf500cec1498f5dbca2c8f807
Justin Moon <mail@justinmoon.com>
2019-08-31 15:59:45 -0500

    Run Junction in Electron

---
+++ ELECTRON.md
@@ -0,0 +1,12 @@
+# To run:
+
+In addition to python instructions in README
+
+```
+npm install
+npm start
+```
+
+## Notes
+
+- Had to do [https://github.com/hundredrabbits/Orca/issues/91#issuecomment-508750882](this hack) to get electron to run on arch ...
---
+++ index.js
@@ -0,0 +1,45 @@
+const {app, BrowserWindow, shell} = require('electron')
+const {PythonShell} = require('python-shell')
+
+function createWindow () {
+  // run junction
+  PythonShell.run('ui.py', {}, function  (err, results)  {
+   if  (err)  console.log(err);
+  });
+
+  // open browser windows, visit junction
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nativeWindowOpen: true,
+    },
+  })
+
+  window.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
+    if (frameName === 'modal') {
+      // open window as modal
+      event.preventDefault()
+      Object.assign(options, {
+        modal: true,
+        parent: mainWindow,
+        width: 100,
+        height: 100
+      })
+      event.newGuest = new BrowserWindow(options)
+    }
+  })
+  //window.loadFile('index.html')
+  window.loadURL('http://localhost:5000')
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
