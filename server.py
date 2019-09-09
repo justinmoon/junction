652200be4ae8b0b0232f945374d622e98d1e01ad
Will O'Beirne <wbobeirne@gmail.com>
2019-09-08 19:17:05 -0500

    Revert server.py change, add a second wallet.

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from hwilib import commands
 from flask import Flask, jsonify
 from flask_cors import CORS
-import json
 
 app = Flask(__name__)
 CORS(app)
@@ -13,12 +12,6 @@ def home():
 @app.route('/enumerate')
 def enumerate():
     return jsonify(commands.enumerate())
-
-@app.route('/psbt')
-def psbt():
-    with open('data/psbt_decoded.json'):
-        data = json.load(json_file)
-        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
