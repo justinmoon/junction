85d2a7d1e7a6e80e9bad339d6d0c9e3870edee5d
Justin Moon <mail@justinmoon.com>
2019-10-10 20:58:16 -0500

    Modify "device instructions modal" depending on network

--- src/components/DeviceInstructionsModal.tsx
+++ src/components/DeviceInstructionsModal.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalFooter, ModalBody } from 'reactstrap';
-import { AppState } from '../store';
+import { AppState, notNull } from '../store';
 import { toggleDeviceInstructionsModal } from '../store/modal'
 import { connect } from 'react-redux';
+import { selectActiveWallet } from '../store/wallet';
+import { Network } from '../types';
 
 interface DispatchProps {
   toggleDeviceInstructionsModal: typeof toggleDeviceInstructionsModal;
 }
 
 interface StateProps {
+  network: Network;
   open: AppState['modal']['deviceInstructions']['open'];
   deviceType: AppState['modal']['deviceInstructions']['deviceType'];
 }
@@ -17,10 +20,11 @@ type Props = DispatchProps & StateProps
 
 class DeviceInstructionsModal extends React.Component<Props> {
   render() {
-    const { toggleDeviceInstructionsModal, open, deviceType } = this.props;
+    const { network, toggleDeviceInstructionsModal, open, deviceType } = this.props;
     const showTrezor = !deviceType || deviceType === 'trezor';
     const showLedger = !deviceType || deviceType === 'ledger';
     const showColdCard = !deviceType || deviceType === 'coldcard';
+    const onMainnet = network === Network.mainnet;
     return (
 			<Modal isOpen={open} toggle={() => toggleDeviceInstructionsModal()}>
 				<ModalHeader toggle={() => toggleDeviceInstructionsModal()}>Device Instructions</ModalHeader>
@@ -31,7 +35,7 @@ class DeviceInstructionsModal extends React.Component<Props> {
               <ul>
                 <li>Plug in</li>
                 <li>Trezor T: enter PIN on device</li>
-                <li>Trezor One: close this modal and click the "Unlock" button in the "Add Signers" section, which will prompt you to enter your passphrase in Junction</li>
+                <li>Trezor One: close this modal and click the "Unlock" button, which will prompt you to enter your PIN in Junction</li>
               </ul>
             </div>}
           {showColdCard &&
@@ -39,25 +43,25 @@ class DeviceInstructionsModal extends React.Component<Props> {
               <h3 className="text-center mb-3">ColdCard</h3>
               <ul>
                 <li>Plug in and enter the PIN on the device</li>
-                <li>Set coldcard to "testnet" mode in "Settings &gt; Blockchain &gt; Testnet: BTC"</li>
+                <li>Check your ColdCard is on the right network: "Settings &gt; Blockchain &gt; {onMainnet ? 'Bitcoin' : 'Testnet: BTC'}</li>
               </ul>
             </div>}
           {showLedger && 
             <div>
               <h3 className="text-center mb-3">Ledger</h3>
               <ul>
-                <li>Enter PIN on the device and navigate to the testnet app on your Ledger</li>
+                <li>Enter PIN on the device and navigate to the {onMainnet ? 'Bitcoin' : 'Testnet'} app on your Ledger</li>
                 <li>If it's unlocked, in testnet app and doesn't show up -- unplug and try again.</li>
-                <li>If you don't have the testnet app installed on your Ledger:</li>
+                {!onMainnet && <div><li>If you don't have the testnet app installed on your Ledger:</li>
                 <ul>
-                  <li>Open "Ledger Live" desktop app (you can install "Ledger Live" <a href="https://support.ledger.com/hc/en-us/articles/360006395553-Download-and-install-Ledger-Live">here).</a></li>
+                  <li>Open "Ledger Live" desktop app (you can install "Ledger Live" <a target="_external" href="https://support.ledger.com/hc/en-us/articles/360006395553-Download-and-install-Ledger-Live">here</a>).</li>
                   <li>Click the settings gear icon in top right.</li>
                   <li>Click "Experimental features" tab at top.</li>
                   <li>Enable "Developer mode".</li>
                   <li>Click the "Manager" tab on left.</li>
                   <li>Enter Ledger pin and accept the "Allow Ledger manager?" prompt on device screen.</li>
                   <li>Search for and install the "Bitcoin Test" app.</li>
-                </ul>
+                </ul></div>}
               </ul>
             </div>}
 				</ModalBody>
@@ -71,6 +75,7 @@ class DeviceInstructionsModal extends React.Component<Props> {
 
 export const mapStateToProps = (state: AppState) => {
 	return {
+    network: notNull(selectActiveWallet(state)).node.network,
     open: state.modal.deviceInstructions.open,
     deviceType: state.modal.deviceInstructions.deviceType,
 	}
--- src/types/wallet.ts
+++ src/types/wallet.ts
@@ -1,5 +1,20 @@
 import { DeviceType } from './device';
 
+export enum Network {
+  mainnet = 'mainnet',
+  testnet = 'testnet',
+  regtest = 'regtest',
+}
+
+export interface Node {
+  host: string;
+  port: string;
+  user: string;
+  password: string;
+  wallet_name: string;
+  network: Network;
+}
+
 export interface Signer {
   name: string;
   type: DeviceType;
@@ -18,4 +33,5 @@ export interface Wallet {
   psbts: any;      // FIXME
   history: any;
   coins: any;
+  node: Node;
 }
