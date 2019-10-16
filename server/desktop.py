7f74eda031400a98d07981351848661135867c58
Justin Moon <justin@buidlbootcamp.com>
2019-10-16 07:22:08 -0500

    Improve desktop.py

--- server/desktop.py
+++ server/desktop.py
@@ -1,24 +1,59 @@
+import sys
 from threading import Thread
+from time import sleep
 
 import webview
 
 from server import server
 
+def url_ok(url, port):
+    # Use httplib on Python 2
+    try:
+        from http.client import HTTPConnection
+    except ImportError:
+        from httplib import HTTPConnection
+
+    try:
+        conn = HTTPConnection(url, port)
+        conn.request('GET', '/')
+        r = conn.getresponse()
+        return r.status == 200
+    except:
+        print('Server not started')
+        return False
+
+HOST = '127.0.0.1'
+PORT = 37128
+
 server = Thread(
     target=server.run, 
     kwargs={
-        'host': '0.0.0.0',
-        'port': 37128,
+        'host': HOST,
+        'port': PORT,
         'threaded': False,
     },
 )
+server.daemon = True
 server.start()
+
+print('starting server')
+
+while not url_ok(HOST, PORT):
+    sleep(.1)
+
+print('server started')
 
 webview.create_window(
     'Junction',
-    'http://localhost:37128',
-    min_size=(600, 400),
-    text_select=True,
+    f'http://{HOST}:{PORT}',
+    # min_size=(600, 400),
+    # text_select=True,
 )
-webview.start(debug=True)
+# webview.start(debug=True)
 
+if sys.platform == 'win32':
+    print('starting cef')
+    webview.start(http_server=True, gui='cef')
+else:
+    webview.start(http_server=True, gui='qt')
+webview.start(http_server=True)
